be43b57476fdf542429416f9bdad11dcfe54686c
takePon <mildsevenlover@mail.goo.ne.jp>
2012-10-14 19:37:39 +0900

    firstcommit

---
+++ firstMain/firstMain/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstMain
+//
+//  Created by 智大 武原 on 12/10/14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ firstMain/firstMain/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  firstMain
+//
+//  Created by 智大 武原 on 12/10/14.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+//test
+@end
